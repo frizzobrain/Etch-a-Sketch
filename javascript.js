ab9730c995511ce7e922b3d3f677b491a5c5c9de
FrizzoBrain <frizzobrain@gmail.com>
2026-01-20 15:22:19 +0100

    Reset the grid changing number of squares

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
 const button = document.getElementById("squarePerRowButton");
 button.addEventListener("click", function(e){
     e.preventDefault();
-    console.log(button.form.squarePerRowId.value);
+    createDiv(button.form.squarePerRowId.value);
 })
 
 const container = document.querySelector(".container");
 function createDiv(val){
+    container.querySelectorAll('*').forEach(n => n.remove());
     for (let i=0; i<(val*val); i++){
     const div = document.createElement("div");
     const lato = 960/val;
     div.style.height= lato+"px";
     div.style.width = lato+"px";
     div.style.backgroundColor = "yellow";
-    // div.style.border = "1px solid black";
+    div.style.border = "1px solid black";
     container.appendChild(div);
 }
 }
-createDiv(2);
+
