d3d8e8d9a19c6d3e01fcb154ea3fa8eef15d0b25
FrizzoBrain <frizzobrain@gmail.com>
2026-01-20 13:14:39 +0100

    Create Function to generate the first div

--- javascript.js
+++ javascript.js
@@ -3,3 +3,15 @@ button.addEventListener("click", function(e){
     e.preventDefault();
     console.log(button.form.squarePerRowId.value);
 })
+
+const container = document.querySelector(".container");
+function createDiv(val){
+    const div = document.createElement("div");
+    const lato = 960/val;
+    div.style.height= lato+"px";
+    div.style.width = lato+"px";
+    div.style.backgroundColor = "black";
+    container.appendChild(div);
+}
+
+createDiv(20);
