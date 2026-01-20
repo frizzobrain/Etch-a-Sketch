69aee1ca1537a2b40b3d00b1d4caf9a32e6a9dfa
FrizzoBrain <frizzobrain@gmail.com>
2026-01-20 15:34:53 +0100

    Add the eventlistener that color tiles on mouseover

--- javascript.js
+++ javascript.js
@@ -6,14 +6,17 @@ button.addEventListener("click", function(e){
 
 const container = document.querySelector(".container");
 function createDiv(val){
+    //reset grid first, removing all the previously created node
     container.querySelectorAll('*').forEach(n => n.remove());
+    //create the new nodes
     for (let i=0; i<(val*val); i++){
     const div = document.createElement("div");
     const lato = 960/val;
     div.style.height= lato+"px";
     div.style.width = lato+"px";
     div.style.backgroundColor = "yellow";
-    div.style.border = "1px solid black";
+    // div.style.border = "1px solid black";
+    div.addEventListener("mouseenter",e =>e.target.style.backgroundColor = "black");
     container.appendChild(div);
 }
 }
