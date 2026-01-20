3768a5fe4707b7cd84dc45de7f0d8900e17946fc
FrizzoBrain <frizzobrain@gmail.com>
2026-01-20 16:54:50 +0100

    add random color on tiles on mouseover

--- javascript.js
+++ javascript.js
@@ -4,6 +4,18 @@ button.addEventListener("click", function(e){
     createDiv(button.form.squarePerRowId.value);
 })
 
+const createGrid = document.querySelector("#createGrid");
+createGrid.addEventListener("click", function(e){
+    //e.preventDefault();
+    let numberSelected = prompt ("how many squares per row do you want?");
+    while (isNaN(numberSelected)|| numberSelected <1 || numberSelected>=100 ){
+        console.log(numberSelected);
+        numberSelected = prompt ("choose a number between 1 and 100")
+    } 
+        createDiv(numberSelected);
+    
+});
+
 const container = document.querySelector(".container");
 function createDiv(val){
     //reset grid first, removing all the previously created node
@@ -16,8 +28,16 @@ function createDiv(val){
     div.style.width = lato+"px";
     div.style.backgroundColor = "yellow";
     // div.style.border = "1px solid black";
-    div.addEventListener("mouseenter",e =>e.target.style.backgroundColor = "black");
+    div.addEventListener("mouseenter",e =>e.target.style.backgroundColor = getColor());
     container.appendChild(div);
 }
 }
 
+function getColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
