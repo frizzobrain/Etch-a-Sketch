fdedbcb0d253387a01078641e9ff3f92ead887a3
FrizzoBrain <frizzobrain@gmail.com>
2026-01-20 15:10:35 +0100

    Create a full table giving how many squares i want for each side

--- javascript.js
+++ javascript.js
@@ -6,12 +6,14 @@ button.addEventListener("click", function(e){
 
 const container = document.querySelector(".container");
 function createDiv(val){
+    for (let i=0; i<(val*val); i++){
     const div = document.createElement("div");
     const lato = 960/val;
     div.style.height= lato+"px";
     div.style.width = lato+"px";
-    div.style.backgroundColor = "black";
+    div.style.backgroundColor = "yellow";
+    // div.style.border = "1px solid black";
     container.appendChild(div);
 }
-
-createDiv(20);
+}
+createDiv(2);
